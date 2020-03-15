dfb814a4c1a4ee29a5ec3b02dddedf44892a8743
angiegigishang <shangxingyu@163.com>
2020-03-15 21:34:54 +0800

    react redux reducer

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,86 +1,33 @@
 import React, { Component, Fragment } from 'react';
-import TodoItem from './TodoItem.js';
-import axios from 'axios';
-import './style.css';
+import 'antd/dist/antd.css';
+import { Input,  Button, List } from 'antd';
+import store from './store';
 
 class TodoList extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      inputValue: '',
-      list: []
-    }
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
+    this.state = store.getState();
+    console.log(store.getState());
   }
 
   render() {
     return (
-      <Fragment>
-        <div>
-          <label htmlFor="insertArea">input value</label>
-          <input 
-            value={this.state.inputValue}
-            id="insertArea"
-            className='input'
-            onChange={this.handleInputChange}/>
-          <button onClick={this.handleBtnClick}>submit</button>
-        </div>
-        <ul>
-          { this.getTodoItem() }
-        </ul>
-      </Fragment>
+      <div>
+        <Input value={this.state.inputValue} placeholder="todo info" style={{width: '300px', marginRight: '10px'}}/>
+        <Button type="primary">submit</Button>
+        <List
+          bordered
+          style={{marginTop:'10px', width:'300px'}}
+          dataSource={this.state.list}
+          renderItem={item => (
+            <List.Item>
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
     )
-  }
-
-  componentDidMount() {
-    axios.post('http://192.168.99.106:8080/user/list', {
-      'HEAD': {'aaaaa':''},
-      'BODY': {'aaaaa':''},
-      'Content-Type':'application/json'
-    })
-      .then((res) => {console.log(res)})
-      .catch(() => {alert('error')})
-  }
-
-  getTodoItem() {
-    return this.state.list.map((item, index) => {
-      return (
-        <TodoItem content={item} 
-                  index={index} 
-                  key={index}
-                  deleteItem={this.handleItemDelete}></TodoItem>
-      )
-    })
-  }
-
-  handleInputChange(e) {
-    const value = e.target.value;
-    this.setState(()=> ({
-        inputValue: value 
-    }))
-    // this.setState({
-    //   inputValue: e.target.value
-    // })
-  }
-
-  handleBtnClick() {
-    this.setState((prevState) => ({
-      list: [...prevState.list, prevState.inputValue],
-      inputValue: ''
-    })) 
-  }
-
-  handleItemDelete(index) {
-    //immutable
-    //state的值不允许我们做任何改变
-    this.setState((prevState) =>{
-      const list = [...prevState.list];
-      list.splice(index, 1);
-      return {list};
-    })
   }
 }
 
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import TodoList from './TodoList';
 //jsx
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
 
