049854f0204682e5de4be609f469e4e497b0f524
angiegigishang <shangxingyu@163.com>
2020-03-16 21:55:07 +0800

    add actionCreator

--- todolist/src/store/reducer.js
+++ todolist/src/store/reducer.js
@@ -1,4 +1,4 @@
-import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM }  from './actionTypes'
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM }  from './actionTypes';
 
 const defaultState = {
     inputValue: '',
