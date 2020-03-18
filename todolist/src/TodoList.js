996a20349694d2a894701d41a2c14b18abb29341
angiegigishang <shangxingyu@163.com>
2020-03-18 23:09:04 +0800

    get statedata before react-redux

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,58 +1,27 @@
-import React, { Component, Fragment } from 'react';
-import 'antd/dist/antd.css';
+import React, { Component } from 'react';
 import store from './store';
-import { getInputChangeAction, getAddItemAction, getDeleteItemAction, initListAction, getTodoList } from './store/actionCreators';
-import TodoListUI from './TodoListUI';
-import axios from 'axios';
 
 class TodoList extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = store.getState();
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleStoreChange = this.handleStoreChange.bind(this);
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
-    store.subscribe(this.handleStoreChange);
-  }
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+    }
 
-  render() {
-    return (
-      <TodoListUI 
-          inputValue={this.state.inputValue}
-          handleInputChange={this.handleInputChange}
-          handleBtnClick={this.handleBtnClick}
-          list={this.state.list}
-          handleItemDelete={this.handleItemDelete}
-      />
-    )
-  }
-
-  componentDidMount() {
-    const action = getTodoList();
-    store.dispatch(action);
-    console.log(action);
-  }
- 
-  handleInputChange(e) {
-    const action = getInputChangeAction(e.target.value);
-    store.dispatch(action);
-  }
-
-  handleStoreChange() {
-    this.setState(store.getState())
-  }
-
-  handleBtnClick() {
-    const action = getAddItemAction();
-    store.dispatch(action);
-  }
-
-  handleItemDelete(index) {
-    const action = getDeleteItemAction(index);
-    store.dispatch(action);
-  }
+    render() {
+        return (
+            <div>
+                <div>
+                    <input value={this.state.inputValue}/>
+                    <button>submit</button>
+                </div>
+                <ul>
+                    <li>dell</li>
+                </ul>
+            </div>
+            
+        )
+    }
 }
 
 export default TodoList;
