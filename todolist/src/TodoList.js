49abc92b251ce3128b616e1b562d3682f2749b3b
angiegigishang <shangxingyu@163.com>
2020-03-18 23:24:25 +0800

    add redux thunk connect

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import store from './store';
+import { connect } from 'react-redux';
 
 class TodoList extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = store.getState();
-    }
 
     render() {
         return (
             <div>
                 <div>
-                    <input value={this.state.inputValue}/>
+                    <input value={this.props.inputValue} onChange={this.props.changeInputValue}/>
                     <button>submit</button>
                 </div>
                 <ul>
                     <li>dell</li>
                 </ul>
             </div>
-            
         )
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        inputValue: state.inputValue
     }
 }
 
-export default TodoList;
+//props.dispatch
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeInputValue(e) {
+            const action = {
+                type: 'change_input_value',
+                value: e.target.value
+            }
+            dispatch(action);
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
