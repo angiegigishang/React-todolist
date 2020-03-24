9f3011c0edcae98785cf128af586b61f4d6de0ee
angiegigishang <shangxingyu@163.com>
2020-03-24 23:00:55 +0800

    component header finish

--- todolist/src/common/header/index.js
+++ todolist/src/common/header/index.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import {
-    HeaderWrapper
+    HeaderWrapper,
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button
 } from './style';
 
 class Header extends Component {
     render() {
-        return 
-            (<HeaderWrapper>header</HeaderWrapper>)
+        return (
+            <HeaderWrapper>
+                <Logo href="/"/>
+                <Nav>
+                    <NavItem className='left'>首页</NavItem>
+                    <NavItem className='left'>下载APP</NavItem>
+                    <NavItem className='right'>登陆</NavItem>
+                    <NavItem className='right'>Aa</NavItem>
+                    <NavSearch>
+                    
+                    </NavSearch>
+                </Nav>
+                <Addition>
+                    <Button className='writting'>写文章</Button>
+                    <Button className='reg'>注册</Button>    
+                </Addition>
+            </HeaderWrapper>
+        )
     }
 }
 
