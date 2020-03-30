51b9d4df991071da253cc3441b0780b4275fc02c
angiegigishang <shangxingyu@163.com>
2020-03-30 21:14:13 +0800

    update redux-immutable

--- todolist/src/common/header/index.js
+++ todolist/src/common/header/index.js
@@ -54,7 +54,8 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.get('header').get('focused')
+        focused: state.getIn(['header', 'focused'])
+        //focused: state.get('header').get('focused')
     }
 }
 
