8ee76066f426560343693c805e3309dc89fd9085
angiegigishang <shangxingyu@163.com>
2020-03-31 23:00:06 +0800

    code better writting style

--- todolist/src/common/header/index.js
+++ todolist/src/common/header/index.js
@@ -21,7 +21,8 @@ import {GlobalStyle} from '../.././statics/iconfont/iconfont.js';
 
 class Header extends Component {
     getListArea() {
-        if(this.props.focused) {
+        const { focused, list } = this.props;
+        if(focused) {
             return (
                 <SearchInfo>
                     <SearchInfoTitle>
@@ -30,7 +31,7 @@ class Header extends Component {
                     </SearchInfoTitle>
                     <SearchInfoList>
                         {
-                            this.props.list.map((item) => {
+                            list.map((item) => {
                                 return <SearchInfoItem key={item}>{item}</SearchInfoItem>
                             })
                             
@@ -44,6 +45,7 @@ class Header extends Component {
     }
 
     render() {
+        const { focused, handleInputBlur, handleInputFocus } = this.props;
         return (
             <HeaderWrapper>
                 <GlobalStyle/>
@@ -57,17 +59,17 @@ class Header extends Component {
                     </NavItem>
                     <SearchWrapper>
                         <CSSTransition
-                            in={this.props.focused}
+                            in={focused}
                             timeout={200}
                             classNames="slide"
                         >
                             <NavSearch
-                                className={this.props.focused ? 'focused' : ''}
-                                onFocus={this.props.handleInputFocus}
-                                onBlur={this.props.handleInputBlur}
+                                className={focused ? 'focused' : ''}
+                                onFocus={handleInputFocus}
+                                onBlur={handleInputBlur}
                             ></NavSearch>
                         </CSSTransition>        
-                        <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
+                        <i className={focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
                         {this.getListArea()}
                     </SearchWrapper>            
                 </Nav>
