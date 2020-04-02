1df9cc978901feaf64423e23e6033950d2325e12
angiegigishang <shangxingyu@163.com>
2020-04-02 22:21:27 +0800

    divide header components

---
+++ todolist/src/pages/home/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Topic from './components/Topic';
+import List from './components/List';
+import Writer from './components/Writer';
+import Recommend from './components/Recommend';
+import { 
+    HomeWrapper,
+    HomeLeft,
+    HomeRight
+} from './style';
+
+class Home extends Component {
+    render() {
+        return (
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className='banner-img' src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2148318713,3960000066&fm=26&gp=0.jpg"/>
+                    <Topic/>
+                    <List/>
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend/>
+                    <Writer/>
+                </HomeRight>
+            </HomeWrapper>
+        )
+    }
+}
+
+export default Home;
