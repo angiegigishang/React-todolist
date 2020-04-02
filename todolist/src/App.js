fe066727833d0f075fbd9083011f267367cf165a
angiegigishang <shangxingyu@163.com>
2020-04-02 21:52:26 +0800

    add react router

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route} from 'react-router-dom';
 import Header from './common/header';
 import {GlobalRestStyle} from './style.js';
 import store from './store';
@@ -11,8 +12,16 @@ class App extends Component {
         return (
             <Fragment>
                 <Provider store={store}>
-                    <GlobalRestStyle/>
-                    <Header/>
+                    <div>
+                        <GlobalRestStyle/>
+                        <Header/>
+                        <BrowserRouter>
+                            <div>
+                                <Route path='/' exact render={() => <div>home</div>}></Route>
+                                <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+                            </div>
+                        </BrowserRouter>
+                    </div>  
                 </Provider>
             </Fragment>  
         )
