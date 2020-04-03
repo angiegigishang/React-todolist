c96ffe30401058cf507fc4ca72025137fcc5706a
angiegigishang <shangxingyu@163.com>
2020-04-03 23:30:15 +0800

    add topic banner

--- todolist/src/pages/home/components/Topic.js
+++ todolist/src/pages/home/components/Topic.js
@@ -1,9 +1,81 @@
 import React, { Component } from 'react';
+import { TopicWrapper, TopicItem } from '../style';
 
 class Topic extends Component {
     render() {
         return (
-            <div>Topic</div>
+            <TopicWrapper>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img 
+                        src='https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/120/h/120/format/webp'
+                        className='topic-pic'
+                    />
+                    社会热点
+                </TopicItem>
+            </TopicWrapper>
         )
     }
 }
